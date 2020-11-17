75c0567df798377b4125585f4ed0643d1ca385d2
Sokwva <1519181530@qq.com>
2020-11-17 10:01:16 +0800

    First Commit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module acfun-helper-watchlaterSync
 go 1.15
 
 require (
+	github.com/Mrs4s/MiraiGo v0.0.0-20201116095353-c0ec373de5a9 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-playground/validator/v10 v10.4.1 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,53 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
 )
 
 func main() {
+
+	authList := make(map[int]string)
+	var arr1 [1]int
+	var userList []int = arr1[:]
+
 	log.WithFields(log.Fields{
 		"App": "Console",
 	}).Info("Start App...")
+
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
+	// Basic Routes
+	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"response": "pong",
+			"Wellcome": "Hi There.",
+		})
+	})
+	r.GET("/ping", pingpong)
+	r.GET("/regUser/:passToken", func(c *gin.Context) {
+		//, authList map[int]string, userList []int
+		token := c.Param("passToken")
+		fmt.Println(token)
+		var num int
+		if authList != nil && len(userList) != 0 {
+			num = len(userList)
+			authList[num] = token
+		} else {
+			authList[0] = token
+		}
+		finalCode := "acHad56-ed3e0208d2ac" + token
+		log.Infof("User %d Reg Token as %s;", num, finalCode)
+
+		c.JSON(200, gin.H{
+			"status": "ok,Your Code is " + finalCode,
 		})
 	})
 	r.Run()
+}
+
+func pingpong(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"response": "pong",
+	})
 }
